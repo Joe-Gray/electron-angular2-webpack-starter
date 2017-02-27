a960bf1076ac0783058da71bca6af086e4121000
Joe-Gray <joseph.gray@carvana.com>
2017-02-26 20:56:59 -0700

    removed about view

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,14 +1,12 @@
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 import { AdminComponent } from './admin/admin.component';
 import { ManageMarketComponent } from './manage-market/manage-market.component';
 import { SettingsComponent } from './settings/settings.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent},
   { path: 'admin', component: AdminComponent},
   { path: 'manage-market', component: ManageMarketComponent},
   { path: 'settings', component: SettingsComponent}
--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { remote } from 'electron';
 
 @Component({
   selector: 'app-settings',
@@ -10,6 +11,10 @@ export class SettingsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    console.log('Hello Settings');
+    console.log(process.versions.electron);
+    var win = new remote.BrowserWindow({width: 300, height: 350});
+    win.loadURL("https://www.google.com");    
   }
 
 }
